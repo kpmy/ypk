6f0f8db00a492143705d7a517ee481a57697c468
kpmy <petryxa.clever@gmail.com>
2015-01-18 16:15:24 +0300

    вдогонку

--- assert/debug.go
+++ assert/debug.go
@@ -9,11 +9,11 @@ func For(cond bool, code int, msg ...interface{}) {
 	if !cond {
 		switch {
 		case (code >= 20) && (code < 40):
-			e = fmt.Sprintln(code, "precondition violated", msg)
+			e = fmt.Sprintln(code, "precondition violated", fmt.Sprint(msg))
 		case (code >= 40) && (code < 60):
-			e = fmt.Sprintln(code, "subcondition violated", msg)
+			e = fmt.Sprintln(code, "subcondition violated", fmt.Sprint(msg))
 		case (code >= 60) && (code < 80):
-			e = fmt.Sprintln(code, "postcondition violated", msg)
+			e = fmt.Sprintln(code, "postcondition violated", fmt.Sprint(msg))
 		default:
 		}
 		panic(e)
